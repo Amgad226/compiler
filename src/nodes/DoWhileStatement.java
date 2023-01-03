d7c5ca3ef64a2f3c45a8188a5ea2f6dc94a96cbb
Amgad226 <amgad.wr.1@gmail.com>
2023-01-03 22:56:18 +0200

    a

---
+++ src/nodes/DoWhileStatement.java
@@ -0,0 +1,27 @@
+package nodes;
+
+
+
+public class DoWhileStatement extends  Statement {
+    public Condition condition;
+    public Block block;
+    public DoWhileStatement(Condition condition,Block block)
+    {
+        this.condition= condition;
+        this.block = block;
+    }
+
+    @Override
+    public String toString() {
+
+
+        return "do"  + block.toString()  +'\n'+  "while"+'(' + condition.toString() + ')';
+    }
+}
+/*
+  do{
+  }
+ while(true);
+*/
+
+
---
+++ src/nodes/ForStatement.java
@@ -0,0 +1,6 @@
+package nodes;
+
+public class ForStatement extends Statement{
+
+
+}
