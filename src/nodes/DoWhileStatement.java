394209d2aaaf454cf24abb1cfa8c0ffcc2729c33
aseel776 <95752507+aseel776@users.noreply.github.com>
2023-01-04 13:21:19 +0200

    final merge

--- src/nodes/DoWhileStatement.java
+++ src/nodes/DoWhileStatement.java
@@ -1,29 +1,13 @@
 package nodes;
 
-/*
-* */
+public class DoWhileStatement extends WhileStatement{
 
-public class DoWhileStatement extends  Statement {
-    public Condition condition;
-    public Block block;
-    public DoWhileStatement(Condition condition,Block block)
-    {
-        this.condition= condition;
-        this.block = block;
+    public DoWhileStatement(Condition condition, Block block){
+        super(condition, block);
     }
 
     @Override
     public String toString() {
-
-
-        return "do"  + block.toString()  +'\n'+  "while("  + condition.toString() + ");";
+        return "do" + '\n' + block.toString() + "while" + '(' + condition.toString() + ");" ;
     }
 }
-
-/*
-  do{
-  }
- while(true);
-*/
-
-
